adb545625875fbeaac0aac95b6c7d6c4f7e9eee5
tca19 <tca@localhost>
2017-12-05 20:08:46 +0100

    put code in main() to be reused with an import

--- dict-dl/download_definitions.py
+++ dict-dl/download_definitions.py
@@ -28,17 +28,14 @@ import argparse
 import time
 import sys
 
-# STEP 0 : declaration of constants
-##
-
-globalStart = time.time()
+# global variables used (and shared) by all ThreadDown instances
 exitFlag = 0
 counterLock = Lock()
 request_counter  = {"Cam": 0, "Dic": 0, "Col": 0, "Oxf": 0}
 download_counter = {"Cam": 0, "Dic": 0, "Col": 0, "Oxf": 0}
 
-
 class ThreadDown(Thread):
+    """Class representing a thread that download definitions."""
     def __init__(self, dict_name, data_queue, res_queue):
         Thread.__init__(self)
         self.dict_name  = dict_name
@@ -67,6 +64,7 @@ class ThreadDown(Thread):
                                                         " ".join(result)))
 
 class ThreadWrite(Thread):
+    """Class representing a thread that write definitions to a file."""
     def __init__(self, filename, msg_queue):
         Thread.__init__(self)
         self.msg_queue = msg_queue
@@ -87,115 +85,112 @@ class ThreadWrite(Thread):
 
         self.of.close()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("list_words", metavar="list-words", help="""file containing
-                    a list of words to download definitions""")
-args = parser.parse_args()
+def main(filename):
+    # 0. to measure download time + use global to be able to modify exitFlag
+    globalStart = time.time()
+    global exitFlag
 
-# STEP 1 : load all words from the filename parameter
-##
 
-filename = args.list_words
-vocabulary = {}
-with open(filename) as f:
-    for line in f:
-        if not line.strip() in vocabulary:
-            vocabulary[line.strip()] = []
+    # 1. load all words from the filename argument
+    vocabulary = {}
+    with open(filename) as f:
+        for line in f:
+            if not line.strip() in vocabulary:
+                vocabulary[line.strip()] = []
 
-vocabulary_size = len(vocabulary)
-# add "-definitions" before the file extension
-output_fn = splitext(filename)[0] + "-definitions.txt"
-print("Writing definitions in", output_fn)
+    vocabulary_size = len(vocabulary)
+    # add "-definitions" before the file extension to create output filename
+    output_fn = splitext(filename)[0] + "-definitions.txt"
+    print("Writing definitions in", output_fn)
 
-# STEP 2 : populate all the queues with words
-##
 
-queue_Cam = Queue() # no init val -> infinite size
-queue_Dic = Queue()
-queue_Col = Queue()
-queue_Oxf = Queue()
-queue_msg = Queue()
-threads = []
+    # 2. create queues containing all words to fetch (1 queue per dictionary)
+    queue_Cam = Queue() # no init val -> infinite size
+    queue_Dic = Queue()
+    queue_Col = Queue()
+    queue_Oxf = Queue()
+    queue_msg = Queue()
 
-for w in vocabulary:
-    queue_Cam.put(w)
-    queue_Dic.put(w)
-    queue_Col.put(w)
-    queue_Oxf.put(w)
+    for w in vocabulary:
+        queue_Cam.put(w)
+        queue_Dic.put(w)
+        queue_Col.put(w)
+        queue_Oxf.put(w)
 
-# STEP 3 : spawn the threads, SPAWN THEM ALL!
-##
 
-# Start the writer thread.
-thread_writer = ThreadWrite(output_fn, queue_msg)
-thread_writer.start()
-threads.append(thread_writer)
+    # 3. create threads
+    threads = []
+    thread_writer = ThreadWrite(output_fn, queue_msg)
+    thread_writer.start()
+    threads.append(thread_writer)
 
-# In my case, fastest fetching was achieved with 12 threads per core,
-# but since there are 4 types of threads (1 for each dictionary), it
-# gives a number of thread per type equal to :
-# (NB_CORE * 12 ) / 4 = NB_CORE * 3
-NB_THREAD = cpu_count() * 3
+    # in my case, fastest fetching was achieved with 12 threads per core,
+    # but since there are 4 types of threads (1 for each dictionary), it
+    # gives a number of thread per type equal to :
+    # (NB_CORE * 12 ) / 4 = NB_CORE * 3
+    NB_THREAD = cpu_count() * 3
 
-# Start all the download threads.
-for x in range(NB_THREAD):
-    thread_Cam = ThreadDown("Cam", queue_Cam, queue_msg)
-    thread_Cam.start()
-    thread_Dic = ThreadDown("Dic", queue_Dic, queue_msg)
-    thread_Dic.start()
-    thread_Col = ThreadDown("Col", queue_Col, queue_msg)
-    thread_Col.start()
-    thread_Oxf = ThreadDown("Oxf", queue_Oxf, queue_msg)
-    thread_Oxf.start()
+    # start all the download threads.
+    for x in range(NB_THREAD):
+        thread_Cam = ThreadDown("Cam", queue_Cam, queue_msg)
+        thread_Cam.start()
+        thread_Dic = ThreadDown("Dic", queue_Dic, queue_msg)
+        thread_Dic.start()
+        thread_Col = ThreadDown("Col", queue_Col, queue_msg)
+        thread_Col.start()
+        thread_Oxf = ThreadDown("Oxf", queue_Oxf, queue_msg)
+        thread_Oxf.start()
 
-    threads.append(thread_Cam)
-    threads.append(thread_Dic)
-    threads.append(thread_Col)
-    threads.append(thread_Oxf)
+        threads.append(thread_Cam)
+        threads.append(thread_Dic)
+        threads.append(thread_Col)
+        threads.append(thread_Oxf)
 
-# STEP 4 : wait until all queues are empty. Show progress.
-#          Tell threads show is over.
-##
 
-percent = 0
+    # 4. wait until all queues are empty, show progress, terminate all threads
+    percent = 0
+    while not queue_msg.empty() or \
+          not queue_Cam.empty() or \
+          not queue_Dic.empty() or \
+          not queue_Col.empty() or \
+          not queue_Oxf.empty():
 
-while not queue_msg.empty() or \
-      not queue_Cam.empty() or \
-      not queue_Dic.empty() or \
-      not queue_Col.empty() or \
-      not queue_Oxf.empty():
+        # the expected number of fetched words is the number of words
+        # in the vocabulary times 4 because we are using 4 dictionaries.
+        # The number of fetched words is simply the sum of each counter.
+        tmp = sum(request_counter.values()) / (4.0 * vocabulary_size) * 100
+        tmp = int(tmp) + 1
+        if tmp != percent:
+            print('\r{0}%'.format(tmp), end="")
+            percent = tmp
 
-    # The expected number of fetched words is the number of words
-    # in the vocabulary times 4 because we are using 4 dictionaries.
-    # The number of fetched words is simply the sum of each counter.
-    tmp = sum(request_counter.values()) / (4.0 * vocabulary_size) * 100
-    tmp = int(tmp) + 1
-    if tmp != percent:
-        print('\r{0}%'.format(tmp), end="")
-        percent = tmp
+    exitFlag = 1
+    # we only wait thread_writer to join because this is the most important
+    # thread in the code and we are only interested in what the program
+    # is writing. And sometimes, some ThreadDown are stuck in a
+    # connection timeout and cause the entire code to freeze and never
+    # terminate.
+    print("\nWaiting thread_writer to join.")
+    thread_writer.join()
 
-exitFlag = 1
 
-# We only wait thread_writer to join because this is the most important
-# thread in the code and we are only interested in what the program
-# is writing. And sometimes, some ThreadDown are stuck in a
-# connection timeout and cause the entire code to freeze and never
-# terminate.
-print("\nWaiting thread_writer to join.")
-thread_writer.join()
-#for t in threads:
-#    t.join()
+    # 5. get total time and some results infos.
+    print("Total time: {:.2f} sec\n".format(time.time() - globalStart))
+    print("S T A T S (# successful download / # requests)")
+    print("==============================================")
+    for dic in sorted(request_counter.keys()):
+        print("{}   {}/{}  ({:.1f}%)".format(dic, download_counter[dic],
+          request_counter[dic], download_counter[dic] * 100 / request_counter[dic]))
 
-# STEP 5 : get total time and some results infos.
-##
+    print("\nVocabulary size:", vocabulary_size)
+    print("Results written in", output_fn)
 
-print("Total time:", time.time() - globalStart, "sec\n")
-print("S T A T S (# successful download / # requests)")
-print("==============================================")
-for dic in sorted(request_counter.keys()):
-    print("{}   {}/{}  ({:.1f}%)".format(dic, download_counter[dic],
-      request_counter[dic], download_counter[dic] * 100 / request_counter[dic]))
 
-print("\nVocabulary size:", vocabulary_size)
-print("Results written in", output_fn)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("list_words", metavar="list-words",
+           help="file containing a list of words to download definitions")
+    args = parser.parse_args()
 
+    print(args)
+    main(args.list_words)
