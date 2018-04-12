6185fdf66684a23e0507b7dc7951c93f511b4272
tca19 <30314448+tca19@users.noreply.github.com>
2018-04-12 23:27:29 +0200

    download April 2018 dump, not July 2017

--- wiki-dl.sh
+++ wiki-dl.sh
@@ -21,14 +21,14 @@
 
 DATA_DIR=./data
 
-echo "Download English Wikipedia dump of July 2017..."
-URL=https://dumps.wikimedia.org/enwiki/20170701/enwiki-20170701-pages-articles-multistream.xml.bz2
+echo "Download English Wikipedia dump of April 2018..."
+URL=https://dumps.wikimedia.org/enwiki/20180401/enwiki-20180401-pages-articles-multistream.xml.bz2
 time wget -qO- $URL | bzip2 -d | perl wiki-parser.pl > "$DATA_DIR/enwiki-full"
 echo "Done."
 echo
 
-echo "Creating enwiki-50M and enwiki-200M..."
-head -c 296093483 "$DATA_DIR/enwiki-full" > "$DATA_DIR/enwiki-50M"
-head -c 1164406185 "$DATA_DIR/enwiki-full" > "$DATA_DIR/enwiki-200M"
-echo "Done."
-echo
+#echo "Creating enwiki-50M and enwiki-200M..."
+#head -c 296093483 "$DATA_DIR/enwiki-full" > "$DATA_DIR/enwiki-50M"
+#head -c 1164406185 "$DATA_DIR/enwiki-full" > "$DATA_DIR/enwiki-200M"
+#echo "Done."
+#echo
